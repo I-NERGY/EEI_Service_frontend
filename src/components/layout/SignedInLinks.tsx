b63ee22648d49024f034fb3850ae78b7bb72da0e
gkorbakis <gkorbakis@epu.ntua.gr>
2022-11-08 16:34:13 +0200

    SignedInLinks.tsx, index.css various updates

--- src/components/layout/SignedInLinks.tsx
+++ src/components/layout/SignedInLinks.tsx
@@ -3,11 +3,15 @@ import {
     NavigateFunction,
     Location
 } from 'react-router-dom';
+
+
+import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from "@mui/material/ListItemText";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
+import Typography from "@mui/material/Typography";
 
 interface Props {
     navigate: NavigateFunction,
@@ -18,22 +22,33 @@ interface Props {
 const SignedInLinks = ({navigate, location, handleSignOut}: Props) => {
     return (
         <>
-            <ListItemButton
-                onClick={() => navigate('/user/profile')}
-                key={'My Profile'}
-                className={location.pathname === '/user/profile' ? 'menuItemActive' : ''}
-            >
-                <ListItemIcon>{<AccountCircleIcon color="secondary"/>}</ListItemIcon>
-                <ListItemText primary={'My Profile'}></ListItemText>
-            </ListItemButton>
+            <ListItem disablePadding
+                      className={location.pathname === '/user/profile' ? 'menuItemActive' : ''}
+                      sx={{
+                          borderRadius: '10px !important', margin: 1, width: '95%'
+                      }}>
+                <ListItemButton onClick={() => navigate('/user/profile')}>
+                    <ListItemIcon>{<AccountCircleIcon color="secondary"/>}</ListItemIcon>
+                    <ListItemText primary={
+                        <Typography fontWeight={500} fontSize={17} align={'center'}
+                                    color={location.pathname === '/user/profile' ? 'white' : 'normal'}>
+                            {'My Profile'}
+                        </Typography>}/>
+                </ListItemButton>
+            </ListItem>
 
-            <ListItemButton
-                onClick={handleSignOut}
-                key={'Sign Out'}
-            >
-                <ListItemIcon>{<LogoutOutlinedIcon color="secondary"/>}</ListItemIcon>
-                <ListItemText primary={'Sign Out'}></ListItemText>
-            </ListItemButton>
+            <ListItem disablePadding
+                      sx={{
+                          borderRadius: '10px !important', margin: 1, width: '95%'
+                      }}>
+                <ListItemButton onClick={handleSignOut}>
+                    <ListItemIcon>{<LogoutOutlinedIcon color="secondary"/>}</ListItemIcon>
+                    <ListItemText primary={
+                        <Typography fontWeight={500} fontSize={17} align={'center'}>
+                            {'Sign Out'}
+                        </Typography>}/>
+                </ListItemButton>
+            </ListItem>
         </>
     );
 }
