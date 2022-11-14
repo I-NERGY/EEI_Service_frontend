f42dc1974eeb304188a91eea549611db2f8828dc
gkorbakis <gkorbakis@epu.ntua.gr>
2022-11-14 17:43:13 +0200

    InvestmentSelect.tsx some fixes

--- src/pages/InvestmentSelect.tsx
+++ src/pages/InvestmentSelect.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 import Link from "@mui/material/Link";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import Breadcrumb from "../components/layout/Breadcrumb";
 
+import Breadcrumb from "../components/layout/Breadcrumb";
 import InvestmentSelectQuickInfo from "../components/InvestmentSelect/InvestmentSelectQuickInfo";
 
 const breadcrumbs = [
@@ -27,6 +27,11 @@ const InvestmentSelect = () => {
             <Breadcrumb breadcrumbs={breadcrumbs} welcome_msg={'Energy Efficiency Investment De-Risking'}/>
             <Container maxWidth={'xl'} sx={{my: 5}}>
                 <InvestmentSelectQuickInfo energyClass={'classAPlusPlus'} thermalTransmittance={1.9} energyConsumption={500}/>
+            </Container>
+
+            <Container maxWidth={'xl'} sx={{my: 5, display: 'flex'}}>
+                <Typography variant={'h4'} fontWeight={'bold'} sx={{mb: 3, flexGrow: 1}}>Energy Efficiency Measures</Typography>
+                <Typography variant={'h5'} fontWeight={'bold'} sx={{mb: 3}}>Total Cost: 3500€</Typography>
             </Container>
         </>
     );
