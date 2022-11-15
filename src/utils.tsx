cd09ec022933a38e67ff844ae0d0c110540df051
gkorbakis <gkorbakis@epu.ntua.gr>
2022-11-15 16:21:13 +0200

    utils.tsx addition

---
+++ src/utils.tsx
@@ -0,0 +1,16 @@
+export const handleEnergyClass = (className: string) => {
+    if (className === 'classAPlusPlus') return 'classAPlusPlusSelected'
+    if (className === 'classAPlus') return 'classAPlusSelected'
+    if (className === 'classA') return 'classASelected'
+    if (className === 'classB') return 'classBSelected'
+    if (className === 'classC') return 'classCSelected'
+    if (className === 'classD') return 'classDSelected'
+    if (className === 'classE') return 'classESelected'
+}
+
+export const handleUColor = (value: number) => {
+    if (value < 1.4) return '#63AA5A'
+    if (value >= 1.4 && value < 2) return '#FBB900'
+    if (value >= 2 && value < 2.2) return '#FB8800'
+    if (value >= 2.2) return '#E30613'
+}
