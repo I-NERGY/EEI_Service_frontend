f2885a3641ed6d3ac188070af80dc84f8cde4d5b
gkorbakis <gkorbakis@epu.ntua.gr>
2023-02-28 17:04:03 +0200

    EnergyMeasuresEdit.tsx some fixes

--- src/pages/EnergyMeasuresEdit.tsx
+++ src/pages/EnergyMeasuresEdit.tsx
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import {styled} from '@mui/material/styles';
 
 import Link from "@mui/material/Link";
@@ -15,12 +16,14 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Snackbar from '@mui/material/Snackbar';
-import MuiAlert, { AlertProps } from '@mui/material/Alert';
+import MuiAlert, {AlertProps} from '@mui/material/Alert';
+import Box from "@mui/material/Box";
 
 import EditAttributesIcon from '@mui/icons-material/EditAttributes';
 import RestartAltIcon from '@mui/icons-material/RestartAlt';
 
 import Breadcrumb from "../components/layout/Breadcrumb";
+import Loading from "../components/layout/Loading";
 
 const StyledTableCell = styled(TableCell)(({theme}) => ({
     [`&.${tableCellClasses.head}`]: {
@@ -69,35 +72,43 @@ const EnergyMeasuresEdit = () => {
         setEditFailure(false)
     }
 
-    const measuresList = [
-        {id: 0, title: 'Gas condensing boiler', cost: 14000, checked: false},
-        {id: 1, title: '2-Pane window', cost: 220, checked: false},
-        {id: 2, title: 'Assembly of internal wall vacuum insulation panels', cost: 148, checked: false},
-        {id: 3, title: 'Attic insulation with bulk stone wool', cost: 50, checked: false},
-        {id: 4, title: 'Attic insulation with ECO wool', cost: 60, checked: false},
-        {id: 5, title: 'Building connection to local heat network', cost: 18000, checked: false},
-        {id: 6, title: 'Construction of a two-pipe system', cost: 40000, checked: false},
-        {id: 7, title: 'Entrance doors', cost: 300, checked: false},
-        {id: 8, title: 'Facade insulation with polystyrene foam', cost: 1000, checked: false},
-        {id: 9, title: 'Facade insulation with stone wool', cost: 800, checked: false},
-        {id: 10, title: 'Gas condensing boiler for the preparation of hot water', cost: 1800, checked: false},
-    ]
-
+    const [measuresList, setMeasuresList] = useState<any[]>([])
     const [measuresListTemp, setMeasuresListTemp] = useState([...measuresList])
+
+    useEffect(() => {
+        axios.get('energy_measures')
+            .then(response => {
+                setMeasuresList(response.data)
+                setMeasuresListTemp(response.data)
+            })
+    }, [])
 
     const handleCostChange = (id: number, e: React.ChangeEvent<any>) => {
         let arrayTemp = measuresList.map(obj => {
-            if (obj.id === id) {
-                return {...obj, cost: e.target.value}
+            if (obj.energy_measure_id === id) {
+                return {...obj, total_per_unit: e.target.value}
             }
             return obj
         })
         setMeasuresListTemp(arrayTemp)
     }
 
-    const handleReset = () => {
+    const handleReset = (id: number) => {
         setEditSuccess(true)
-        setMeasuresListTemp(measuresList)
+        let arrayTemp = measuresListTemp
+        arrayTemp[id].total_per_unit = measuresList[id].total_per_unit
+        setMeasuresListTemp(arrayTemp)
+    }
+
+    // TODO Finalize
+    const handleSave = (id: number) => {
+        let payload = {
+
+        }
+        axios.put(`energy_measures/${id}`, payload)
+            .then(response => {
+
+            })
     }
 
     return (
@@ -110,27 +121,36 @@ const EnergyMeasuresEdit = () => {
                             <TableRow>
                                 <StyledTableCell>
                                     <Typography fontWeight={'bold'} variant={'subtitle1'}>
-                                        Energy Measure title
+                                        Code
                                     </Typography>
                                 </StyledTableCell>
                                 <StyledTableCell align="left">
                                     <Typography fontWeight={'bold'} variant={'subtitle1'}>
-                                        Cost
+                                        Place
                                     </Typography>
                                 </StyledTableCell>
-                                <StyledTableCell align="right"></StyledTableCell>
-                                <StyledTableCell align="right"></StyledTableCell>
+                                <StyledTableCell align="left">
+                                    <Typography fontWeight={'bold'} variant={'subtitle1'}>
+                                        Total cost per unit
+                                    </Typography>
+                                </StyledTableCell>
+                                <StyledTableCell align="right">{void (0)}</StyledTableCell>
+                                <StyledTableCell align="right">{void (0)}</StyledTableCell>
                             </TableRow>
                         </TableHead>
+
                         <TableBody>
-                            {measuresListTemp.map(measure => (
-                                <StyledTableRow key={measure.id}>
+                            {measuresListTemp.length > 0 && measuresListTemp.map(measure => (
+                                <StyledTableRow key={measure.energy_measure_id}>
                                     <StyledTableCell component="th" scope="row">
-                                        <Typography variant={'body1'}>{measure.title}</Typography>
+                                        <Typography variant={'body1'}>{measure.code}</Typography>
+                                    </StyledTableCell>
+                                    <StyledTableCell component="th" scope="row">
+                                        <Typography variant={'body1'}>{measure.place}</Typography>
                                     </StyledTableCell>
                                     <StyledTableCell align="left">
                                         <TextField
-                                            onChange={e => handleCostChange(measure.id, e)}
+                                            onChange={e => handleCostChange(measure.energy_measure_id, e)}
                                             required
                                             fullWidth
                                             id="outlined-required"
@@ -140,18 +160,20 @@ const EnergyMeasuresEdit = () => {
                                                 inputProps: {min: 0},
                                                 startAdornment: <InputAdornment position="start">€</InputAdornment>
                                             }}
-                                            value={measure.cost}
+                                            value={measure.total_per_unit}
                                         />
                                     </StyledTableCell>
                                     <StyledTableCell align="right">
                                         <Button size={'large'} variant="contained" color={'warning'}
-                                                startIcon={<EditAttributesIcon/>} disabled={!measure.cost}>
+                                                onClick={() => handleSave(measure.energy_measure_id)}
+                                                startIcon={<EditAttributesIcon/>} disabled={!measure.total_per_unit}>
                                             SAVE
                                         </Button>
                                     </StyledTableCell>
                                     <StyledTableCell align="right">
                                         <Button size={'large'} variant="contained" color={'success'}
-                                                startIcon={<RestartAltIcon/>} onClick={handleReset}>
+                                                startIcon={<RestartAltIcon/>}
+                                                onClick={() => handleReset(measure.energy_measure_id)}>
                                             RESET
                                         </Button>
                                     </StyledTableCell>
@@ -160,16 +182,20 @@ const EnergyMeasuresEdit = () => {
                         </TableBody>
                     </Table>
                 </TableContainer>
+                {measuresListTemp.length < 1 &&
+                    <Box justifyContent={'center'} alignItems={'center'} p={5}>
+                        <Loading/>
+                    </Box>}
             </Container>
 
             <Snackbar open={editSuccess} autoHideDuration={3000} onClose={handleCloseSnackbar}>
-                <Alert onClose={handleCloseSnackbar} severity="success" sx={{ width: '100%' }}>
+                <Alert onClose={handleCloseSnackbar} severity="success" sx={{width: '100%'}}>
                     Energy Measure cost has been successfully changed!
                 </Alert>
 
             </Snackbar>
             <Snackbar open={editFailure} autoHideDuration={3000} onClose={handleCloseSnackbar}>
-                <Alert onClose={handleCloseSnackbar} severity="error" sx={{ width: '100%' }}>
+                <Alert onClose={handleCloseSnackbar} severity="error" sx={{width: '100%'}}>
                     Something went wrong! Please try again.
                 </Alert>
             </Snackbar>
