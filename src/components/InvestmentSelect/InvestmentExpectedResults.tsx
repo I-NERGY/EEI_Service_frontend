2d589b6d0c9a99040aaeb5d199012a94dc5012d1
gkorbakis <gkorbakis@epu.ntua.gr>
2023-09-05 16:20:14 +0300

    InvestmentExpectedResults.tsx: Multilingual integration

--- src/components/InvestmentSelect/InvestmentExpectedResults.tsx
+++ src/components/InvestmentSelect/InvestmentExpectedResults.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {useNavigate} from "react-router-dom";
-import {handleEnergyClass, handleUColor} from "../../utils";
+import {handleEnergyClass} from "../../utils";
+import {LanguageContext} from "../../context/LanguageContext";
+import {multilingual} from "../../multilingual";
 
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Button from '@mui/material/Button';
 import Divider from "@mui/material/Divider";
 import Stack from "@mui/material/Stack";
+
 
 interface Props {
     energyClass: string,
@@ -17,20 +20,23 @@ interface Props {
 
 const InvestmentExpectedResults = ({energyClass, energyConsumption, totalCost, handleClose}: Props) => {
     let navigate = useNavigate();
+    const {language} = useContext(LanguageContext);
+    const dictionary = language === 'en' ? multilingual.english.selectInvestment : multilingual.latvian.selectInvestment;
 
     return (
         <>
             <Stack direction={'row'} sx={{mt: 2}}>
-                <Typography variant={'h4'} sx={{py: 0}} flexGrow={1}>Expected Results</Typography>
-                <Typography variant={'h5'}>Total cost: {totalCost.toFixed(2)}€</Typography>
+                <Typography variant={'h4'} sx={{py: 0}} flexGrow={1}>{dictionary.expected}</Typography>
+                <Typography variant={'h5'}>{dictionary.totalCost}: {totalCost.toFixed(2)}€</Typography>
             </Stack>
 
             <Divider/>
 
             <Grid container display={'flex'} spacing={5} sx={{py: 3}}>
                 <Grid item xs={12} md={6} display={'flex'} flexDirection={'column'} alignItems={'center'}>
-                    <Typography variant={'h5'} sx={{mb: 'auto'}} fontWeight={'bold'} align={'center'}>Energy
-                        Class</Typography>
+                    <Typography variant={'h5'} sx={{mb: 'auto'}} fontWeight={'bold'} align={'center'}>
+                        {dictionary.energyClass}
+                    </Typography>
                     <div className="energy-class" style={{marginTop: '10px'}}>
                         {/*<span className="classAPlusPlus">A<sup>++</sup></span>*/}
                         {/*<span className="classAPlus">A<sup>+</sup></span>*/}
@@ -49,19 +55,20 @@ const InvestmentExpectedResults = ({energyClass, energyConsumption, totalCost, h
                 {/*                color={() => handleUColor(thermalTransmittance)}>{thermalTransmittance}</Typography>*/}
                 {/*</Grid>*/}
                 <Grid item xs={12} md={6} display={'flex'} flexDirection={'column'} alignItems={'center'}>
-                    <Typography variant={'h5'} sx={{mb: 'auto'}} fontWeight={'bold'} align={'center'}>Total
-                        Energy Consumption:</Typography>
+                    <Typography variant={'h5'} sx={{mb: 'auto'}} fontWeight={'bold'} align={'center'}>
+                        {dictionary.totalConsumption}:
+                    </Typography>
                     <Typography variant={'h3'} my={'auto'} align={'center'}>{energyConsumption} kWh</Typography>
                 </Grid>
             </Grid>
             <Divider/>
             <Stack direction={'row'} sx={{mt: 2}}>
-                <Typography variant={'h6'} flexGrow={1}>Want to try other measures?</Typography>
+                <Typography variant={'h6'} flexGrow={1}>{dictionary.tryOther}</Typography>
                 <Button variant="contained" color="success" sx={{mx: 1}} onClick={() => handleClose()}>
-                    Yes, try other measures.
+                    {dictionary.yesModal}
                 </Button>
                 <Button variant="outlined" color="error" onClick={() => navigate('/building-info')}>
-                    No, back to service page.
+                    {dictionary.noModal}
                 </Button>
             </Stack>
         </>
