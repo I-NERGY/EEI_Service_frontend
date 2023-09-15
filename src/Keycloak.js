55f694d57dd27bc5cef8e3f102eb75d80f7a0197
gkorbakis <gkorbakis@epu.ntua.gr>
2023-09-15 14:00:26 +0300

    Keycloak.js: ICCS deployment

--- src/Keycloak.js
+++ src/Keycloak.js
@@ -1,17 +1,17 @@
 import Keycloak from "keycloak-js";
 
 // local configuration
-const my_keycloak = new Keycloak({
-    "realm": "inergy",
-    "url": "https://oblachek.eu:8443/",
-    "clientId": "uc13"
-})
-
-// ICCS deployment configuration
 // const my_keycloak = new Keycloak({
 //     "realm": "inergy",
 //     "url": "https://oblachek.eu:8443/",
-//     "clientId": "uc13_iccs_deployment"
+//     "clientId": "uc13"
 // })
+
+// ICCS deployment configuration
+const my_keycloak = new Keycloak({
+    "realm": "inergy",
+    "url": "https://oblachek.eu:8443/",
+    "clientId": "uc13_iccs_deployment"
+})
 
 export default my_keycloak
