110dc5806caec4b970297f4023f927a48455f783
gkorbakis <gkorbakis@epu.ntua.gr>
2023-09-20 16:17:11 +0300

    Layout.tsx, index.html: small changes

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -250,7 +250,7 @@ export default function PersistentDrawerLeft({children}: Props) {
                             <MenuIcon/>
                         </IconButton>
                         <Typography variant="h6" noWrap component="div" color={'white'} fontWeight={'bold'}>
-                            I-NERGY UC13 Dashboard
+                            Energy efficiency investments decision support system (EFI-DSS)
                         </Typography>
                         {keycloak.authenticated === true && <React.Fragment>
                             <FormControl sx={{ml: 'auto', minWidth: 120}} size="small" className={'language'}>
